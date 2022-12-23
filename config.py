a3ff3020491454359f79ed7ab33b58561f8180df
KrisStinaK <112583110+KrisStinaK@users.noreply.github.com>
2022-12-23 16:37:56 +0300

    com

--- config.py
+++ config.py
@@ -1,38 +1,34 @@
 from math import *
+from settings import *
 
-# screen configuration
-width = 1200
-height = 800
-half_width = width // 2
-half_height = height // 2
-
-# doom map
-block_size = 100
 text_map = ["WWWWWWWWWWWW",
-            "W..........W",
-            "W..........W",
-            "W.......WWWW",
+            "W...W......W",
+            "W...W......W",
+            "W...W...WWWW",
             "W..........W",
             "WWWW.......W",
             "W..........W",
             "WWWWWWWWWWWW"]
 block_map = set()
+mini_map = set()
 y_block_pos = 0
 for row in text_map:
     x_block_pos = 0
     for column in list(row):
         if column == "W":
-            block_map.add((x_block_pos, y_block_pos))
-        x_block_pos += block_size
-    y_block_pos += block_size
+            block_map.add((x_block_pos * tile, y_block_pos * tile))
+            mini_map.add((x_block_pos * map_tile, y_block_pos * map_tile))
+        x_block_pos += 1
+    y_block_pos += 1
 
 # Ray Casting
 FOV = pi / 3
 half_FOV = FOV / 2
 max_depth = width // block_size
-num_rays = 600
+num_rays = 300
 delta_ray = FOV / (num_rays - 1)
 dist = num_rays / (2 * tan(half_FOV))
 coefficient = dist * block_size
 scale = width // num_rays
+
 
---
+++ settings.py
@@ -0,0 +1,19 @@
+width = 1200
+height = 800
+half_width = width // 2
+half_height = height // 2
+FPS_POS = (width - 65, 5)
+block_size = 100
+tile = 100
+player_pos = (half_width, half_height)
+player_angle = 0
+
+# minimap settings
+map_scale = 5
+map_tile = tile // map_scale
+map_pos = (0, height - height // map_scale)
+
+# color
+green = (0, 100, 0)
+red = (200, 0, 0)
+yellow = (250, 250, 0)
